58cbd4f76b568899b568918d3637404e10b4a7d0
Fernando <ribeiro.fer50@gmail.com>
2022-08-01 13:59:24 -0300

    profile page

--- web/src/pages/profile/index.tsx
+++ web/src/pages/profile/index.tsx
@@ -15,13 +15,47 @@ function Header(){
   );
 }
 
-export function Profile(){
-  const [widthOfMain, setWidthOfMain] = useState("11/12");
-  const myContext = {widthOfMain, setWidthOfMain};
+function MyData(){
+  return (
+    <section className="h-1/4 mt-16 mx-8 rounded-xl flex flex-col  bg-white">
+      <div className="flex m-4">
+        <img className="w-8 mb-4" src="../../static/img/myData.svg" />
+        <span className= "text-2xl text-icon-gold ml-4" >Meus dados</span>
+      </div>
+      <div className="h-2/4 mx-4 rounded-2xl bg-body-light-100 flex flex-col justify-around text-paragraph-light-200">
+        <span className= "text-l ml-4" >Nome: Oscar Ramos</span>
+        <span className= "text-l ml-4" >Data de Nascimento: 01/01/2001</span>
+        <span className= "text-l ml-4" >CPF: 123.456.789-01</span>
+      </div>
+    </section>
+)
+};
 
+function MyAccount(){
+  return (
+    <section className="h-1/4 mt-8 mx-8 rounded-xl flex flex-col  bg-white">
+      <div className="flex m-4">
+        <img className="w-8 mb-4" src="../../static/img/myAccount.svg" />
+        <span className= "text-2xl text-icon-gold ml-4" >Minhas contas correntes</span>
+      </div>
+      <div className="h-2/4 mx-4 mb-4 rounded-2xl bg-body-light-100 flex flex-col justify-around text-paragraph-light-200">
+        <span className= "text-l ml-4" >Agencia: 1234-5</span>
+        <span className= "text-l ml-4" >Conta: 12345-6</span>
+      </div>
+      <div className="h-2/4 mx-4 mb-4 rounded-2xl bg-body-light-100 flex flex-col justify-around text-paragraph-light-200">
+        <span className= "text-l ml-4" >Agencia: 1234-5</span>
+        <span className= "text-l ml-4" >Conta: 12345-6</span>
+      </div>
+    </section>
+)
+};
+
+export function Profile(){
   return (
       <>
               <Header />
+              <MyData />
+              <MyAccount />
       </>
   )
 };
