ad9dd3037ce8ccfe1b44442b9cd82c32f16c76e1
Fernando <ribeiro.fer50@gmail.com>
2022-08-03 11:51:58 -0300

    regex done2

--- web/src/components/RegisterComponent.tsx
+++ web/src/components/RegisterComponent.tsx
@@ -69,41 +69,68 @@ export function RegisterComponent(){
 
       if (name.length < 3 && nameBool == 'true') {
           nameCheck.classList.toggle("hidden");
-          nameInputCheck.classList.toggle("mb-3");
+          nameInputCheck.classList.toggle("mb-4");
           setNameBool('false');
 
       }else if(name.length >= 3 && nameBool == 'false'){
           nameCheck.classList.toggle("hidden");
-          nameInputCheck.classList.toggle("mb-3");
+          nameInputCheck.classList.toggle("mb-4");
           setNameBool('true');;
       }
 
       if (!validateCPF(cpf) && CPFBool == 'true') {
         cpfCheck.classList.toggle("hidden");
-        cpfInputCheck.classList.toggle("mb-3");
+        cpfInputCheck.classList.toggle("mb-4");
         setCPFBool('false');
 
       }else if(validateCPF(cpf) && CPFBool == 'false'){
         cpfCheck.classList.toggle("hidden");
-        cpfInputCheck.classList.toggle("mb-3");
+        cpfInputCheck.classList.toggle("mb-4");
         setCPFBool('true');;
       }
 
-      if (!validatePassword(password)) {
-          passCheck.classList.toggle("hidden");
-          passInputCheck.classList.toggle("mb-3");
+      if (!validatePassword(password) && passwordBool == 'true') {
+        passCheck.classList.toggle("hidden");
+        passInputCheck.classList.toggle("mb-4");
+        setPasswordBool('false');
+
+      }else if(validatePassword(password) && passwordBool == 'false'){
+        passCheck.classList.toggle("hidden");
+        passInputCheck.classList.toggle("mb-4");
+        setPasswordBool('true');
       }
-      if (!(password2 === password)) {
-          pass2Check.classList.toggle("hidden");
-          pass2InputCheck.classList.toggle("mb-5");
+
+      if (!(password2 === password) && password2Bool == 'true') {
+        pass2Check.classList.toggle("hidden");
+        pass2InputCheck.classList.toggle("mb-6");
+        setPassword2Bool('false');
+
+      }else if((password2 === password) && password2Bool == 'false'){
+        pass2Check.classList.toggle("hidden");
+        pass2InputCheck.classList.toggle("mb-6");
+        setPassword2Bool('true');
       }
-      if (!validateEmail(email)) {
+
+      if (!validateEmail(email) && emailBool == 'true') {
         emailCheck.classList.toggle("hidden");
-        emailInputCheck.classList.toggle("mb-3");
+        emailInputCheck.classList.toggle("mb-4");
+        setEmailBool('false');
+
+      }else if(validateEmail(email) && emailBool == 'false'){
+        emailCheck.classList.toggle("hidden");
+        emailInputCheck.classList.toggle("mb-4");
+        setEmailBool('true');
       }
-      if (calcAge(age)<16) {
+
+      if (calcAge(age)<16 && ageBool == 'true') {
         ageCheck.classList.toggle("hidden");
-        ageInputCheck.classList.toggle("mb-3");
+        ageInputCheck.classList.toggle("mb-4");
+        setAgeBool('false');
+
+      }else if(calcAge(age)>=16 && ageBool == 'false'){
+        ageCheck.classList.toggle("hidden");
+        ageInputCheck.classList.toggle("mb-4");
+        setAgeBool('true');
       }
 
   };
@@ -111,17 +138,17 @@ export function RegisterComponent(){
     return (
         <section className="flex flex-col justify-center items-center">
             <h2 className="mt-7 mb-4 text-center text-header-dark dark:text-white">Crie sua conta</h2>
-            <div><input ref={nameInput} onChange={(e)=>setName(e.target.value)} type="text" placeholder="Digite seu nome" className="text-paragraph-dark text-center mb-3 w-240 p-1 border-2 border-gray-300 placeholder-input-placeholder border-input-b rounded-md dark:text-black" /></div>
+            <div><input ref={nameInput} onChange={(e)=>setName(e.target.value)} type="text" placeholder="Digite seu nome" className="text-paragraph-dark text-center mb-4 w-240 p-1 border-2 border-gray-300 placeholder-input-placeholder border-input-b rounded-md dark:text-black" /></div>
             <p ref={nameVerify} className="hidden text-red-600/100 text-xs">Nome maior que 3 caracteres</p>
-            <div><input ref={ageInput} onChange={(e)=>setBirthdate(e.target.value)} type="text" placeholder="Data de nascimento" className="text-paragraph-dark text-center mb-3 w-240 p-1 border-2 border-gray-300 placeholder-input-placeholder border-input-b rounded-md dark:text-black" /></div>
+            <div><input ref={ageInput} onChange={(e)=>setBirthdate(e.target.value)} type="text" placeholder="Data de nascimento" className="text-paragraph-dark text-center mb-4 w-240 p-1 border-2 border-gray-300 placeholder-input-placeholder border-input-b rounded-md dark:text-black" /></div>
             <p ref={ageVerify} className="hidden text-red-600/100 text-xs">Deve ser maior que 16 anos</p>
-            <div><input ref={cpfInput} onChange={(e)=>setCpf(e.target.value)} type="text" placeholder="Digite seu CPF" className="text-paragraph-dark text-center mb-3 w-240 p-1 border-2 border-gray-300 placeholder-input-placeholder border-input-b rounded-md dark:text-black" /></div>
+            <div><input ref={cpfInput} onChange={(e)=>setCpf(e.target.value)} type="text" placeholder="Digite seu CPF" className="text-paragraph-dark text-center mb-4 w-240 p-1 border-2 border-gray-300 placeholder-input-placeholder border-input-b rounded-md dark:text-black" /></div>
             <p ref={cpfVerify} className="hidden text-red-600/100 text-xs">CPF Inválido</p>
-            <div><input ref={emailInput} onChange={(e)=>setEmail(e.target.value)} type="text" placeholder="Digite seu email" className="text-paragraph-dark text-center mb-3 w-240 p-1 border-2 border-gray-300 placeholder-input-placeholder border-input-b rounded-md dark:text-black" /></div>
+            <div><input ref={emailInput} onChange={(e)=>setEmail(e.target.value)} type="text" placeholder="Digite seu email" className="text-paragraph-dark text-center mb-4 w-240 p-1 border-2 border-gray-300 placeholder-input-placeholder border-input-b rounded-md dark:text-black" /></div>
             <p ref={emailVerify} className="hidden text-red-600/100 text-xs">Email Inválido</p>
-            <div><input ref={passInput} onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="Digite sua senha" className="text-paragraph-dark text-center mb-3 w-240 p-1 border-2 border-gray-300 placeholder-input-placeholder border-input-b rounded-md dark:text-black" /></div>
+            <div><input ref={passInput} onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="Digite sua senha" className="text-paragraph-dark text-center mb-4 w-240 p-1 border-2 border-gray-300 placeholder-input-placeholder border-input-b rounded-md dark:text-black" /></div>
             <p ref={passwordVerify} className="hidden text-red-600/100 text-xs">Senha deve conter caracteres do tipo Aa1!</p>
-            <div><input ref={pass2Input} onChange={(e)=>setPassword2(e.target.value)} type="password" placeholder="Confirme sua senha" className="text-paragraph-dark text-center mb-5 w-240 p-1 border-2 border-gray-300 placeholder-input-placeholder rounded-md dark:text-black" /></div>
+            <div><input ref={pass2Input} onChange={(e)=>setPassword2(e.target.value)} type="password" placeholder="Confirme sua senha" className="text-paragraph-dark text-center mb-6 w-240 p-1 border-2 border-gray-300 placeholder-input-placeholder rounded-md dark:text-black" /></div>
             <p ref={password2Verify} className="hidden text-red-600/100 text-xs mb-2">Senhas diferentes</p>
             <div><button onClick={()=>{ApiRequest.createInstanceAxios().register(name,password,email,cpf,birthdate); ErrorTransaction(cpf, password, email, birthdate, name, password2)}} className="px-20 mb-1 w-240 p-1 text-btn-text rounded-md bg-btn-primary-base hover:bg-btn-primary-hover">Cadastrar</button></div>
             <button className="px-24 w-240 p-0.5 text-center dark:text-input-inactive" onClick={()=>c!.setState(<LoginComponent />)}>Entrar</button>
